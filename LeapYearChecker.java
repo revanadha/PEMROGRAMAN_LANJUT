edac0c401006ef8d965bb0065ede7aeac9194f7c
revanadha <revansa888@gmail.com>
2025-10-24 16:11:20 +0800

    push codelab modul 3

--- LeapYearChecker.java
+++ LeapYearChecker.java
@@ -11,7 +11,7 @@ public class LeapYearChecker {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Masukkan tahun (misal: 2000): ");
+            System.out.print("Masukkan tahun (misal: 2000, 2024): ");
 
             // Membaca input tahun dari pengguna
             if (scanner.hasNextInt()) {
