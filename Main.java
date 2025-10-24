ac0189429ebe7dcc3a4ca98d93d3dc123caa5496
revanadha <revansa888@gmail.com>
2025-10-24 13:27:39 +0800

    commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di DocumentationPractice!");
+        System.out.println("Program ini menampilkan pesan sederhana untuk latihan dokumentasi Git dan Java.");
+    }
+}
+
+
